68b168ba0d46e7b3ac105135a165aace146e7b7a
Sebastian Gaviria <vagyriag@hotmail.com>
2019-08-09 20:56:17 -0500

    publicar v1

---
+++ precache-manifest.cdd25d95c85cd61a8d6f1b5b758fb222.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d3a7cee02cdc246341f2b1ae5bc5f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "141637e1e358acc7fe0d",
+    "url": "/static/js/2.a7b6037b.chunk.js"
+  },
+  {
+    "revision": "a4ffe1ca1540a1af913d",
+    "url": "/static/js/main.468ccb30.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
